8b22fda802a90c4762ee4307fc3f77137a9e4a76
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-12-07 16:12:49 +0530

    font add and set video then accecss for set info play list

--- ImageMaker/ImageMakerApp.swift
+++ ImageMaker/ImageMakerApp.swift
@@ -5,13 +5,50 @@
 //  Created by Bhumika Patel on 06/12/24.
 //
 
+//import SwiftUI
+//
+//@main
+//struct ImageMakerApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
 import SwiftUI
 
-@main
-struct ImageMakerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+                
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: PermissionsView())
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
+    
 }
