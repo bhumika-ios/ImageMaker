aa621c2bc5027dcb75255e3d42d0a96bdabd29bd
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-12-07 13:05:38 +0530

    Attach image picker view and access photo camera

---
+++ ImageMaker/Preview Content/Extension/UIView.swift
@@ -0,0 +1,21 @@
+//
+//  UIView.swift
+//  ImageMaker
+//
+//  Created by Bhumika Patel on 07/12/24.
+//
+
+import SwiftUI
+import Combine
+
+extension UIView {
+    func copyView<T: UIView>() -> T {
+        return NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as! T
+    }
+}
+
+extension CALayer {
+    var copied: CALayer {
+        NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as! CALayer
+    }
+}
